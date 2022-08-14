c9cbb2397608021c4ac2b0852f2b901839c3aa70
Jamie Murphy <hello@itsjamie.dev>
2022-08-14 13:01:52 -0700

    shell: Provide basic Shell frontend
    There is very minimal code here right now, as the Shell is intended to
    be managed by a future Plugin/Modularity system. Right now, there's
    really only function definitions, i.e. a Trait.

---
+++ src/shell/mod.rs
@@ -0,0 +1,155 @@
+//! This Implementation, ideally, will purely be callbacks to an FFI.
+//! There should be minimal amounts of code here, any code here is either for debug purposes or in development
+
+use smithay::{
+    desktop::{layer_map_for_output, LayerSurface, PopupManager, Window, WindowSurfaceType},
+    reexports::wayland_server::{protocol::wl_surface::WlSurface, DisplayHandle},
+    utils::{Logical, Point},
+    wayland::{
+        output::Output,
+        seat::{PointerGrabStartData, Seat},
+        shell::{wlr_layer::WlrLayerShellState, xdg::{XdgShellState, PopupSurface, PositionerState}},
+        Serial,
+    },
+};
+
+pub mod workspace;
+
+use crate::state::State;
+
+use self::workspace::Workspace;
+
+pub struct Shell {
+    pub workspaces: Vec<Workspace>,
+    pub outputs: Vec<Output>,
+    pub popups: PopupManager,
+
+    pub pending_windows: Vec<(Window, Seat<State>)>,
+    pub pending_layers: Vec<(LayerSurface, Output, Seat<State>)>,
+
+    // Wayland State
+    pub layer_shell_state: WlrLayerShellState,
+    pub xdg_shell_state: XdgShellState,
+}
+
+impl Shell {
+    pub fn new(dh: &DisplayHandle) -> Self {
+        // TODO Create Workspaces
+
+        Self {
+            workspaces: vec![Workspace::new(0)],
+            outputs: Vec::new(),
+            popups: PopupManager::new(slog_scope::logger()),
+
+            pending_windows: Vec::new(),
+            pending_layers: Vec::new(),
+
+            layer_shell_state: WlrLayerShellState::new::<State, _>(dh, slog_scope::logger()),
+            xdg_shell_state: XdgShellState::new::<State, _>(dh, slog_scope::logger()),
+        }
+    }
+
+    pub fn outputs(&self) -> impl Iterator<Item = &Output> {
+        self.outputs.iter()
+    }
+
+    pub fn add_output(&mut self, output: &Output) {
+        self.outputs.push(output.clone());
+        remap_output(
+            output,
+            &mut self.workspaces,
+            None,
+            0,
+            output.current_location(),
+        );
+    }
+
+    pub fn active_workspace(&self) -> &Workspace {
+        &self.workspaces.get(0).unwrap()
+    }
+
+    pub fn active_workspace_mut(&mut self) -> &mut Workspace {
+        &mut self.workspaces[0]
+    }
+
+    pub fn refresh_outputs(&mut self) {
+        let workspace = &mut self.workspaces[0];
+        for output in self.outputs.iter() {
+            workspace
+                .space
+                .map_output(output, output.current_location());
+        }
+    }
+
+    pub fn refresh(&mut self, dh: &DisplayHandle) {
+        let workspace = &mut self.workspaces[0];
+        workspace.refresh(dh);
+
+        for output in &self.outputs {
+            let mut map = layer_map_for_output(output);
+            map.cleanup(dh);
+        }
+    }
+
+    pub fn space_for_window_mut(&mut self, surface: &WlSurface) -> Option<&mut Workspace> {
+        self.workspaces.iter_mut().find(|workspace| {
+            workspace
+                .space
+                .window_for_surface(surface, WindowSurfaceType::ALL)
+                .is_some()
+        })
+    }
+
+    pub fn map_layer(&mut self, _layer_surface: &LayerSurface, _dh: &DisplayHandle) {
+        // DENO FUNCTION
+    }
+
+    pub fn map_window(&mut self, _window: &Window, _output: &Output, _dh: &DisplayHandle) { 
+        // DENO FUNCTION
+    }
+
+    pub fn move_request(
+        &mut self,
+        _window: &Window,
+        _seat: &Seat<State>,
+        _serial: Serial,
+        _start_data: PointerGrabStartData,
+    ) {
+        // DENO FUNCTION
+    }
+
+    pub fn set_focus(
+        &mut self,
+        _dh: &DisplayHandle,
+        _surface: Option<&WlSurface>,
+        _active_seat: &Seat<State>,
+        _serial: Option<Serial>,
+    ) {
+        // DENO FUNCTION
+    }
+
+    pub fn update_active<'a>(&mut self, _seats: impl Iterator<Item = &'a Seat<State>>) {
+        // DENO FUNCTION
+    }
+
+    pub fn unconstrain_popup(&self, _surface: &PopupSurface, _positioner: &PositionerState) {
+        // DENO FUNCTION
+    }
+}
+
+fn remap_output(
+    output: &Output,
+    spaces: &mut [Workspace],
+    old: impl Into<Option<usize>>,
+    new: impl Into<Option<usize>>,
+    pos: impl Into<Option<Point<i32, Logical>>>,
+) {
+    if let Some(old) = old.into() {
+        let old_space = &mut spaces[old].space;
+        old_space.unmap_output(output);
+    }
+    if let Some(new) = new.into() {
+        let new_space = &mut spaces[new].space;
+        new_space.map_output(output, pos.into().expect("new requires pos"));
+    }
+}
---
+++ src/shell/workspace.rs
@@ -0,0 +1,113 @@
+//! This Implementation, ideally, will purely be callbacks to an FFI.
+//! There should be minimal amounts of code here, any code here is either for debug purposes or in development
+
+use std::collections::HashMap;
+
+use smithay::{
+    desktop::{Kind, Space, Window},
+    reexports::{
+        wayland_protocols::xdg::shell::server::xdg_toplevel::{self, ResizeEdge},
+        wayland_server::DisplayHandle,
+    },
+    utils::IsAlive,
+    wayland::{
+        output::Output,
+        seat::{PointerGrabStartData, Seat},
+        Serial,
+    },
+};
+
+use crate::state::State;
+
+pub struct Workspace {
+    pub idx: u8,
+    pub space: Space,
+    pub fullscreen: HashMap<String, Window>,
+}
+
+impl Workspace {
+    pub fn new(idx: u8) -> Self {
+        Self {
+            idx,
+            space: Space::new(slog_scope::logger()),
+            fullscreen: HashMap::new(),
+        }
+    }
+
+    pub fn refresh(&mut self, dh: &DisplayHandle) {
+        let outputs = self.space.outputs().collect::<Vec<_>>();
+        let dead_windows = self
+            .fullscreen
+            .iter()
+            .filter(|(name, _)| !outputs.iter().any(|o| o.name() == **name))
+            .map(|(_, w)| w)
+            .cloned()
+            .collect::<Vec<_>>();
+        for window in dead_windows {
+            self.unfullscreen_request(&window);
+        }
+        self.fullscreen.retain(|_, w| w.alive());
+        self.space.refresh(dh);
+    }
+
+    pub fn maximize_request(&mut self, _window: &Window, _output: &Output) {}
+
+    pub fn unmaximize_request(&mut self, _window: &Window) {}
+
+    pub fn resize_request(
+        &mut self,
+        _window: &Window,
+        _seat: &Seat<State>,
+        _serial: Serial,
+        _start_data: PointerGrabStartData,
+        _edges: ResizeEdge,
+    ) {
+    }
+
+    pub fn fullscreen_request(&mut self, window: &Window, output: &Output) {
+        if self.fullscreen.contains_key(&output.name()) {
+            return;
+        }
+
+        #[allow(irrefutable_let_patterns)]
+        if let Kind::Xdg(xdg) = &window.toplevel() {
+            xdg.with_pending_state(|state| {
+                state.states.set(xdg_toplevel::State::Fullscreen);
+                state.size = Some(
+                    output
+                        .current_mode()
+                        .map(|m| m.size)
+                        .unwrap_or((0, 0).into())
+                        .to_f64()
+                        .to_logical(output.current_scale().fractional_scale())
+                        .to_i32_round(),
+                );
+            });
+
+            xdg.send_configure();
+            self.fullscreen.insert(output.name(), window.clone());
+        }
+    }
+
+    pub fn unfullscreen_request(&mut self, window: &Window) {
+        if self.fullscreen.values().any(|w| w == window) {
+            #[allow(irrefutable_let_patterns)]
+            if let Kind::Xdg(xdg) = &window.toplevel() {
+                xdg.with_pending_state(|state| {
+                    state.states.unset(xdg_toplevel::State::Fullscreen);
+                    state.size = None;
+                });
+                xdg.send_configure();
+            }
+            self.fullscreen.retain(|_, w| w != window);
+        }
+    }
+
+    pub fn get_fullscreen(&self, output: &Output) -> Option<&Window> {
+        if !self.space.outputs().any(|o| o == output) {
+            return None;
+        }
+
+        self.fullscreen.get(&output.name()).filter(|w| w.alive())
+    }
+}
